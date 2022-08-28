1025433a7dcb0a4bd18627010a654d1d2a43264c
abduh batubara <muhammadabdu089@gmai.com>
2022-08-28 23:04:09 +0700

    Building & Re-using Components

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+// nama function harus diawalin huruf besar
+function Todo() {
+  return (
+    <div className="card">
+      <h2>Title</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
