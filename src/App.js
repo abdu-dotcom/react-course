363572562a480137cb5bb6dee58e312a28964cdc
abduh batubara <muhammadabdu089@gmai.com>
2022-08-29 16:43:53 +0700

    Passing Data With Props & Dynamic Content

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>My Todos</h1>
-      <Todo/>
+      <Todo text="Learn React" number="1" description="Lorem 1"/>
+      <Todo text="Master React" number="2" description="Lorem 2"/>
+      <Todo text="Explore the full React course" number="3"  description="Lorem 3"/>
     </div>
   );
 }
