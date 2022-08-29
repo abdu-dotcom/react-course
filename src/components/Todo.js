fc7e63aa2632047345f2b512269b99b9d0fc1c60
abduh batubara <muhammadabdu089@gmai.com>
2022-08-29 16:44:05 +0700

    Handling Events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,16 @@
 // nama function harus diawalin huruf besar
-function Todo() {
+function Todo(props) {
+
+  const buttonDelete = () => { 
+    console.log("Click botton" + props.number);
+    console.log(props.text);
+    console.log(props.description);
+  }
   return (
     <div className="card">
-      <h2>Title</h2>
+      <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={buttonDelete}>Delete</button>
       </div>
     </div>
   );
