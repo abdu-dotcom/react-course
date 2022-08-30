72ffc65a19de69eec48bbad078579f84b8ae6885
abduh batubara <muhammadabdu089@gmai.com>
2022-08-30 16:59:36 +0700

    41. Preparing The App For Http Requests & Adding a Backend

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,10 +2,14 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 // berfungsi sebagai menampilkan halaman menambahkan meetups
 function NewMeetupPage() {
+
+  function addMeetupHandler() {
+    
+  }
   return (
     <section>
       <h1> New Meet up Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
   );
 }
